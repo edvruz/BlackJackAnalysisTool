3d8cde1eeb5958eaae8238a377f7bdad64d963d6
edvruz <edvinas.ruzgus@gmail.com>
2017-03-12 22:07:12 +0200

    entities

---
+++ BlackJackAnalysisTool/BJAT.Common.Enums/HeroActionsEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace BJAT.Common.Enums
+{
+    public enum HeroActionsEnum
+    {
+        Hit = 1,
+        Double = 2,
+        Split = 3,
+        Stand = 4,
+        Insurance = 5,
+        GiveUp = 6
+    }
+}
---
+++ BlackJackAnalysisTool/BJAT.Data/Entities/Deal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using BJAT.Common.Enums;
+
+namespace BJAT.Data.Entities
+{
+    public class Deal
+    {
+        public int DealId { get; set; }
+        public double InitialBet { get; set; }
+        public int CountOfHands { get; set; }
+        public double TotalBet { get; set; }
+        public double TotalPayout { get; set; }
+        public double Profit { get; set; }
+        public int Mistakes { get; set; }
+
+        public virtual List<Hand> HeroHands { get; set; } = new List<Hand>();
+        public virtual List<Card> DealerCards { get; set; } = new List<Card>();
+        public virtual List<Hand> OtherHands { get; set; } = new List<Hand>();
+        public virtual List<HeroActionsEnum> HeroActions { get; set; } = new List<HeroActionsEnum>();
+    }
+}
